ea7d6ce4eee76c58ea8942617b89377960860e89
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-06 23:09:45 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -24,3 +24,12 @@ BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth>'2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg=weight_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg=weight_kg * -1 WHERE weight_kg<0;
+COMMIT;
+SELECT * FROM animals;
