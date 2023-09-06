ff4891a1bcfccef7acff3e38ead0cb0bcc516990
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-06 22:29:01 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,9 @@ SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
 SELECT * FROM animals WHERE neutered=TRUE;
 SELECT * FROM animals WHERE name!='Gabumon';
 SELECT * FROM animals WHERE weight_kg>=10.4 AND weight_kg<=17.3;
+
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
