4df3d8d4456d5eb73e8a7f2393b1eb5d32494bfe
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-07 00:20:46 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -33,3 +33,10 @@ ROLLBACK TO SP1;
 UPDATE animals SET weight_kg=weight_kg * -1 WHERE weight_kg<0;
 COMMIT;
 SELECT * FROM animals;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts=0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered,AVG(escape_attempts) AS avg_escapes FROM animals GROUP BY neutered ORDER BY avg_escapes DESC LIMIT 1;
+SELECT species,MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species,AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
