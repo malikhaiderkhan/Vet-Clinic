460a651e101270ab0315930f49a092ddc7776c61
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-07 23:57:58 +0500

    insert data in owners table

--- data.sql
+++ data.sql
@@ -13,4 +13,12 @@ VALUES ('Charmander','2020-02-08',0,FALSE,-11),
        ('Blossom','1998-10-13',3,TRUE,17),
        ('Ditto','2022-05-14',4,TRUE,22);
 
+INSERT INTO owners(full_name,age)
+       VALUES ('Sam Smith',34),
+       ('Jennifer Orwell',19),
+       ('Bob',45),
+       ('Melody Pond',77),
+       ('Dean Winchester',14),
+       ('Jodie Whittaker',38);
+
 INSERT INTO species(name) VALUES ('Pokemon'),('Digimon');
