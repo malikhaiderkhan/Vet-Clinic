677413738b3c62fba5047058d0783bb30c128952
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-08 20:40:33 +0500

    insert data into specializations table

--- data.sql
+++ data.sql
@@ -37,3 +37,9 @@ INSERT INTO vets(name,age,date_of_graduation)
        ('Maisy Smith',26,'2019-01-17'),
        ('Stephanie Mendez',64,'1981-05-04'),
        ('Jack Harkness',38,'2008-06-08');
+
+INSERT INTO specializations(species_id,vets_id)
+       VALUES (1,1),
+       (2,3),
+       (1,3),
+       (2,4);
