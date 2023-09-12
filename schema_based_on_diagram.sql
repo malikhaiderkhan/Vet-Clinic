7112e5917e03bfa860fbf7ea81ebb1553b3be297
Malik Haider Khan <malikhaiderkhan81@gmail.com>
2023-09-12 23:44:13 +0500

    create table medical_histories

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,13 @@
 CREATE TABLE patients(
   id INT GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(250),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at timestamp,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(250),
+  PRIMARY KEY (id)
 );
